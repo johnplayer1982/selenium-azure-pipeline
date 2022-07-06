44d20305765b93e7a6cfc287e60af17e990e92ae
John Player <johnplayer@IT00468-John-Player.local>
2022-07-06 07:34:56 +0100

    Adds teardown to set the test as pass or fail

--- Regression/run.py
+++ Regression/run.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import os, breadcrumbs
+import os, requests, breadcrumbs
 
 username = os.getenv("CBT_USERNAME")
 authkey = os.getenv("CBT_AUTHKEY")
+
+api_session = requests.Session()
+api_session.auth = (username, authkey)
+test_result = None
 
 caps = {
  'platform': 'Windows',
@@ -24,4 +28,10 @@ except AssertionError as e:
     test_result = 'fail'
     raise
 
+print("Done with session %s" % driver.session_id)
 driver.quit()
+# Here we make the api call to set the test's score.
+# Pass it it passes, fail if an assertion fails, unset if the test didn't finish
+if test_result is not None:
+    api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + driver.session_id,
+        data={'action':'set_score', 'score':test_result})
