ae2fa7f92d081a8bb23c241bd3bdd99990c5a7a3
John Player <johnplayer@IT00468-John-Player.local>
2022-07-06 08:35:43 +0100

    Adds article list as python list

---
+++ Lib/article_list.py
@@ -0,0 +1,11 @@
+def get_articles():
+    article_list = [
+        "/en/homes/renting/financial-responsibilities-if-you-rent-out-a-property",
+        "/en/homes/renting/rent-a-room-scheme-how-it-works-and-tax-rules",
+        "/en/everyday-money/types-of-credit/choosing-and-applying-for-a-credit-card",
+        "/en/pensions-and-retirement/taking-your-pension/looking-after-your-partner-or-dependants-in-retirement",
+        "/en/family-and-care/student-and-graduate-money/financial-support-further-education",
+        "/en/pensions-and-retirement/state-pension/how-national-insurance-credits-can-help-boost-your-state-pension",
+        "/en/family-and-care/death-and-bereavement/sorting-out-the-estate-when-there-isnt-a-will",
+    ]
+    return article_list
--- Regression/breadcrumbs.py
+++ Regression/breadcrumbs.py
@@ -1,9 +1,15 @@
 from importlib.machinery import SourceFileLoader
 
-dismisscookie = SourceFileLoader('getcookiefile', '../Lib/dismisscookie.py').load_module()
-
 def runTest(baseUrl, driver):
+
+    dismisscookie = SourceFileLoader('getcookiefile', '../Lib/dismisscookie.py').load_module()
+    articlelist = SourceFileLoader('getarticlelist', '../Lib/article_list.py').load_module()
+
     testArticle = "{baseUrl}/en/jp-test/how-to-choose-the-right-bank-account".format(baseUrl=baseUrl)
     driver.get(testArticle)
     dismisscookie.dismissCookieBanner(driver)
-    print('- Cookie banner dismissed')
+    articlelist = articlelist.get_articles()
+    for article in articlelist:
+        iteration_url = "{baseUrl}{article}".format(baseUrl=baseUrl, article=article)
+        driver.get(iteration_url)
+        print('\n Visiting {}'.format(iteration_url))
