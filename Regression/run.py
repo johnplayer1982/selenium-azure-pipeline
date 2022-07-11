5cdddc0edfd048974a02441252753d46dd2e0b82
John Player <johnplayer1982@msn.com>
2022-07-11 09:06:24 +0100

    Splitting templates to new session

--- Regression/run.py
+++ Regression/run.py
@@ -3,11 +3,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os, requests
 
+# Components
 import cookies
 import breadcrumbs
 import accordion_carousel
 import article_feedback
-import blog_post
 import callouts
 import chat
 import emergency_banner
@@ -17,8 +17,6 @@ import header
 import home_hero
 import icon_anchor
 import images
-import subcategory_article_list
-import subcategory_page
 import local_navigation
 import navigation
 import overview_card
@@ -31,8 +29,8 @@ authkey = os.getenv("CBT_AUTHKEY")
 api_session = requests.Session()
 api_session.auth = (username, authkey)
 test_result = None
-release = "Azure Staging Components"
 build = "1.8.4"
+release = "Azure Staging Components - {}".format(build)
 baseUrl = "https://test.moneyhelper.org.uk"
 
 caps = {
@@ -50,27 +48,24 @@ driver = webdriver.Remote(
 
 tests = {
     "Cookies" : cookies,
-    # "Breadcrumbs" : breadcrumbs,
-    # "Accordion Carousel" : accordion_carousel,
-    # "Article Feedback" : article_feedback,
-    # "Blog Post" : blog_post,
-    # "Callouts" : callouts,
-    # "Chat" : chat,
-    # "Emergency Banner" : emergency_banner,
-    # "Footer Follow" : footer_follow,
-    # "Global Card" : global_card,
-    # "Header" : header,
-    # "Home Hero" : home_hero,
-    # "Icon Anchor" : icon_anchor,
-    # "Images" : images,
-    # "Subcategory Article List" : subcategory_article_list,
-    # "Subcategory Page" : subcategory_page,
-    # "Local Navigation" : local_navigation,
-    # "Navigation" : navigation,
-    # "Overview Card" : overview_card,
-    # "Search" : search,
-    # "Section Hero" : section_hero,
-    # "SEO" : seo,
+    "Breadcrumbs" : breadcrumbs,
+    "Accordion Carousel" : accordion_carousel,
+    "Article Feedback" : article_feedback,
+    "Callouts" : callouts,
+    "Chat" : chat,
+    "Emergency Banner" : emergency_banner,
+    "Footer Follow" : footer_follow,
+    "Global Card" : global_card,
+    "Header" : header,
+    "Home Hero" : home_hero,
+    "Icon Anchor" : icon_anchor,
+    "Images" : images,
+    "Local Navigation" : local_navigation,
+    "Navigation" : navigation,
+    "Overview Card" : overview_card,
+    "Search" : search,
+    "Section Hero" : section_hero,
+    "SEO" : seo,
 }
 
 try:
@@ -97,7 +92,7 @@ import search
 api_session = requests.Session()
 api_session.auth = (username, authkey)
 test_result = None
-release = "Azure Staging Search"
+release = "Azure Staging Search - {}".format(build)
 
 caps = {
     'name': '{}'.format(release),
@@ -118,6 +113,53 @@ tests = {
 
 try:
     search.runTest(baseUrl, driver)
+    test_result = 'pass'
+except AssertionError as e:
+    test_result = 'fail'
+    raise
+
+print("Done with session %s" % driver.session_id)
+driver.quit()
+# Here we make the api call to set the test's score.
+# Pass it it passes, fail if an assertion fails, unset if the test didn't finish
+if test_result is not None:
+    api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + driver.session_id,
+        data={'action':'set_score', 'score':test_result})
+
+# Templates
+import blog_post
+import subcategory_page
+import subcategory_article_list
+
+api_session = requests.Session()
+api_session.auth = (username, authkey)
+test_result = None
+release = "Azure Staging Templates - {}".format(build)
+
+caps = {
+    'name': '{}'.format(release),
+    'build': '{}'.format(build),
+    'platform': 'Windows',
+    'browserName': 'Edge',
+    'screenResolution' : '1920x1080',
+    'record_video' : 'true'
+}
+
+driver = webdriver.Remote(
+    command_executor="http://%s:%s@hub.crossbrowsertesting.com/wd/hub"%(username, authkey),
+    desired_capabilities=caps)
+
+tests = {
+    "Blog Post" : blog_post,
+    "Subcategory Page" : subcategory_page,
+    "Subcategory Article List" : subcategory_article_list,
+}
+
+try:
+    for key, value in tests.items():
+        print('Testing {}'.format(key))
+        value.runTest(baseUrl, driver)
+        print('End of {} test\n'.format(key))
     test_result = 'pass'
 except AssertionError as e:
     test_result = 'fail'
