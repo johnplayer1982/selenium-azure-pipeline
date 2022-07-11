1802de822b2a187c7f776ccaa7b6fa26cf4efbc9
John Player <johnplayer1982@msn.com>
2022-07-11 07:44:48 +0100

    Splits search from components at timing out at 30mins

--- Regression/run.py
+++ Regression/run.py
@@ -32,11 +32,13 @@ authkey = os.getenv("CBT_AUTHKEY")
 api_session = requests.Session()
 api_session.auth = (username, authkey)
 test_result = None
-release = "Azure Selenium Staging"
+release = "Azure Staging Components"
+build = "1.8.4"
+baseUrl = "https://test.moneyhelper.org.uk"
 
 caps = {
     'name': '{}'.format(release),
-    'build': '1.8.4',
+    'build': '{}'.format(build),
     'platform': 'Windows',
     'browserName': 'Chrome',
     'screenResolution' : '1920x1080',
@@ -47,30 +49,76 @@ driver = webdriver.Remote(
     command_executor="http://%s:%s@hub.crossbrowsertesting.com/wd/hub"%(username, authkey),
     desired_capabilities=caps)
 
-baseUrl = "https://test.moneyhelper.org.uk"
 tests = {
     "Cookies" : cookies,
-    "Breadcrumbs" : breadcrumbs,
-    "Accordion Carousel" : accordion_carousel,
-    "Article Feedback" : article_feedback,
-    "Blog Post" : blog_post,
-    "Callouts" : callouts,
-    "Chat" : chat,
-    "Emergency Banner" : emergency_banner,
-    "Footer Follow" : footer_follow,
-    "Global Card" : global_card,
-    "Header" : header,
-    "Home Hero" : home_hero,
-    "Icon Anchor" : icon_anchor,
-    "Images" : images,
-    "Subcategory Article List" : subcategory_article_list,
-    "Subcategory Page" : subcategory_page,
-    "Local Navigation" : local_navigation,
-    "Navigation" : navigation,
-    "Overview Card" : overview_card,
+    # "Breadcrumbs" : breadcrumbs,
+    # "Accordion Carousel" : accordion_carousel,
+    # "Article Feedback" : article_feedback,
+    # "Blog Post" : blog_post,
+    # "Callouts" : callouts,
+    # "Chat" : chat,
+    # "Emergency Banner" : emergency_banner,
+    # "Footer Follow" : footer_follow,
+    # "Global Card" : global_card,
+    # "Header" : header,
+    # "Home Hero" : home_hero,
+    # "Icon Anchor" : icon_anchor,
+    # "Images" : images,
+    # "Subcategory Article List" : subcategory_article_list,
+    # "Subcategory Page" : subcategory_page,
+    # "Local Navigation" : local_navigation,
+    # "Navigation" : navigation,
+    # "Overview Card" : overview_card,
+    # "Search" : search,
+    # "Section Hero" : section_hero,
+    # "SEO" : seo,
+}
+
+try:
+    for key, value in tests.items():
+        print('Testing {}'.format(key))
+        value.runTest(baseUrl, driver)
+        print('End of {} test\n'.format(key))
+    test_result = 'pass'
+except AssertionError as e:
+    test_result = 'fail'
+    raise
+
+print("Done with session %s" % driver.session_id)
+driver.quit()
+# Here we make the api call to set the test's score.
+# Pass it it passes, fail if an assertion fails, unset if the test didn't finish
+if test_result is not None:
+    api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + driver.session_id,
+        data={'action':'set_score', 'score':test_result})
+
+# Search
+
+username = os.getenv("CBT_USERNAME")
+authkey = os.getenv("CBT_AUTHKEY")
+
+api_session = requests.Session()
+api_session.auth = (username, authkey)
+test_result = None
+release = "Azure Staging Search"
+build = "1.8.4"
+baseUrl = "https://test.moneyhelper.org.uk"
+
+caps = {
+    'name': '{}'.format(release),
+    'build': '{}'.format(build),
+    'platform': 'Windows',
+    'browserName': 'Edge',
+    'screenResolution' : '1920x1080',
+    'record_video' : 'true'
+}
+
+driver = webdriver.Remote(
+    command_executor="http://%s:%s@hub.crossbrowsertesting.com/wd/hub"%(username, authkey),
+    desired_capabilities=caps)
+
+tests = {
     "Search" : search,
-    "Section Hero" : section_hero,
-    "SEO" : seo,
 }
 
 try:
