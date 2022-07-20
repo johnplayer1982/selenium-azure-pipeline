a6256b5c6b2b4f1112646913964d7d321400bba8
John Player <johnplayer1982@msn.com>
2022-07-20 11:18:43 +0100

    Adds article container tests, adds build as an azure var

--- Regression/article_template.py
+++ Regression/article_template.py
@@ -8,15 +8,43 @@ def runTest(baseUrl, driver):
     resize = SourceFileLoader('getresize', '../Lib/resize.py').load_module()
     
     urls = articlelist.get_articles()
+
     for url in urls:
         iterationUrl = "{baseUrl}{url}".format(baseUrl=baseUrl, url=url)
         driver.get(iterationUrl)
         dismisscookie.dismissCookieBanner(driver)
         print("\nVisiting {iterationUrl}".format(iterationUrl=iterationUrl))
+
+        # Measuring sizes of article content containers
+        # Desktop
         resize.resizeDesktop(driver)
         print("- Maximising window")
+        main_content_expected_width = "954px"
+        side_content_expected_width = "318px"
+        article_main_content = driver.find_element(By.CSS_SELECTOR, 'div.cmp-layout-container__article-content-wrapper')
+        article_main_content_width = article_main_content.value_of_css_property('width')
+        assert article_main_content_width == main_content_expected_width
+        print('- Article desktop main content width ok: {}'.format(main_content_expected_width))
+        side_content = driver.find_element(By.CSS_SELECTOR, 'div.cmp-layout-container__article-content--aside')
+        side_content_width = side_content.value_of_css_property('width')
+        assert side_content_width == side_content_expected_width
+        print('- Article desktop side content width ok: {}'.format(side_content_width))
+
+        # Mobile
+        resize.resizeMobile(driver)
+        main_content_expected_width = "451px"
+        side_content_expected_width = "451px"
+        article_main_content = driver.find_element(By.CSS_SELECTOR, 'div.cmp-layout-container__article-content-wrapper')
+        article_main_content_width = article_main_content.value_of_css_property('width')
+        assert article_main_content_width == main_content_expected_width
+        print('- Article mobile main content width ok: {}'.format(main_content_expected_width))
+        side_content = driver.find_element(By.CSS_SELECTOR, 'div.cmp-layout-container__article-content--aside')
+        side_content_width = side_content.value_of_css_property('width')
+        assert side_content_width == side_content_expected_width
+        print('- Article mobile side content width ok: {}'.format(side_content_width))
 
         # Check for the feedback component
+        resize.resizeDesktop(driver)
         feedbackElem = driver.find_element(By.CSS_SELECTOR, ".cmp-voting")
         assert feedbackElem
         print("- Feedback component found")
